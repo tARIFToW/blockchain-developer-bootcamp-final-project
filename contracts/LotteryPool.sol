1fd2d2f689b4e8ec3e03a74c25ae7f21764f3965
tARIFToW <tARIFToW@gmail.com>
2021-10-14 19:12:12 +0100

    restructure contract

--- contracts/LotteryPool.sol
+++ contracts/LotteryPool.sol
@@ -1,52 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.7.6;
 
-contract LotteryPool {
-  
-  uint poolSize;
-  uint ticketPrice;
-  uint ticketHolderCount;
-  uint lotteryId;
-  mapping (uint=> mapping(uint => address payable)) ticketHolders;
+contract LotteryFactory {
 
-  event LogNewTicket(uint _ticketHolderCount, address _lotteryMember, uint balance);
-  event LogLotteryComplete();
+  event NewLottery(uint lotteryId, string name);
 
-  modifier poolFull {
-    require(poolSize == ticketHolderCount);
-    _;
+  struct Lottery {
+    uint id;
+    string name;
+    uint size;
+    uint ticketPrice;
+    uint ticketHolderCount;
+    bool completed;
+    address payable owner;
+    uint ownerCommission;
+    address winner;
   }
 
-  constructor(uint _ticketPrice, uint _poolSize) {
-    poolSize = _poolSize;
-    ticketPrice = _ticketPrice;
-    lotteryId = 0;
+  Lottery[] lotteries;
+  mapping(uint => mapping(uint => address payable)) ticketHolders;
+  uint lotteryCount;
+
+  function createLottery(string memory _name, uint _size, uint _ticketPrice, uint _ownerCommission) public {
+    lotteries[lotteryCount] = Lottery(lotteryCount, _name, _size, _ticketPrice, 0, false, payable(msg.sender), _ownerCommission, address(0));
+  }
+}
+
+contract LotteryTicket is LotteryFactory {
+
+  function _electWinner(Lottery memory _lottery) internal pure returns (uint) {
+    uint winnerId = 1; // use chainlink to generate random id between 0 and lotterySize
+    return winnerId;
   }
 
-  receive() external payable {
-    require(msg.value == ticketPrice);
-    ticketHolders[lotteryId][ticketHolderCount] = payable(msg.sender);
-    ticketHolderCount += 1;
-    emit LogNewTicket(ticketHolderCount, msg.sender, address(this).balance);
-    if (ticketHolderCount == poolSize) {
-      transfer(electWinner());
-      emit LogLotteryComplete();
-      clearLottery();
-    }
+  function _transferFundsToWinner(Lottery memory _lottery, uint _winnerId) internal {
+    uint prize = (_lottery.size * _lottery.ticketPrice) * (100 - _lottery.ownerCommission);
+    (bool success, ) = ticketHolders[_lottery.id][_winnerId].call { value: prize }("");
+    require (success, "Failed to send ether");
   }
 
-  function clearLottery() private poolFull {
-      ticketHolderCount = 0;
-      lotteryId += 1;
-  }
-
-  function electWinner() private poolFull view returns (address payable) {
-    uint randomIndex = 1; // TODO: use safemath to pick random index
-    return ticketHolders[lotteryId][randomIndex];
-  }
-
-  function transfer(address payable _winnerAddress) private poolFull {
-    (bool success, ) = _winnerAddress.call{ value: address(this).balance }("");
+  function _transferFundsToOwner(Lottery memory _lottery) internal {
+    uint commission = _lottery.size * _lottery.ticketPrice * _lottery.ownerCommission;
+    (bool success, ) = _lottery.owner.call{ value: commission }("");
     require(success, "Failed to send ether");
+  }
+
+  function buyTicket(uint _lotteryId) public payable {
+    Lottery storage lottery = lotteries[_lotteryId];
+    require(msg.value == lottery.ticketPrice);
+    require(!lottery.completed);
+    lottery.ticketHolderCount++;
+    if (lottery.ticketHolderCount == lottery.size) {
+      uint winnerId = _electWinner(lottery);
+      _transferFundsToWinner(lottery, winnerId);
+      _transferFundsToOwner(lottery);
+      lottery.completed = true;
+    }
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+dotenv.config();
+const mnemonic = process.env.MNEMONIC;
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -57,14 +60,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/278c03bbb41644a6961b4a64cdf8cd59`),
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -81,7 +84,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
