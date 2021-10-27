72b66f417c5ed43c07499f19a84b3f019b7edcb6
tARIFToW <tARIFToW@gmail.com>
2021-10-27 17:36:24 +0100

    finish tests

--- test/lottery.js
+++ test/lottery.js
@@ -7,6 +7,7 @@ const LotteryTicket = artifacts.require("LotteryTicket");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("LotteryTicket", async (accounts) => {
+  const web3 = new Web3("http://127.0.0.1:8545");
   LotteryTicket.defaults({ gasPrice: 0 });
   it("should have no lotteries when deployed", async () => {
     const lotteryContract = await LotteryTicket.deployed();
@@ -16,14 +17,14 @@ contract("LotteryTicket", async (accounts) => {
 
   it("should create a new lottery", async () => {
     const lotteryContract = await LotteryTicket.deployed();
-    await lotteryContract.createLottery("test", 3, Web3.utils.toWei('1', 'ether'), 10);
+    await lotteryContract.createLottery("test", 2, Web3.utils.toWei('1', 'ether'), 10);
     const lotteries = await lotteryContract.getLotteries();
     return assert.equal(1, lotteries.length);
   })
 
   it("should be able to create 2 lotteries", async () => {
     const lotteryContract = await LotteryTicket.deployed();
-    await lotteryContract.createLottery("test 2", 3, Web3.utils.toWei('1', 'ether'), 10);
+    await lotteryContract.createLottery("test 2", 3, Web3.utils.toWei('1', 'ether'), 10, {from: accounts[1]});
     const lotteries = await lotteryContract.getLotteries();
     return assert.equal(2, lotteries.length);
   })
@@ -41,8 +42,8 @@ contract("LotteryTicket", async (accounts) => {
 
     it("should increase the ticketHolderCount and balance", async () => {
       const lotteryContract = await LotteryTicket.deployed();
-      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[0] });
-      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[1] });
+      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[2] });
+      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[3] });
       const lottery2 = await lotteryContract.getLottery(1);
       assert.equal(lottery2.ticketHolderCount, 2);
       const balance2 = await lotteryContract.getBalance();
@@ -51,15 +52,18 @@ contract("LotteryTicket", async (accounts) => {
 
     it("should complete lottery when size is reached", async () => {
       const lotteryContract = await LotteryTicket.deployed();
-      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[2] });
+      await lotteryContract.buyTicket(1, {value: Web3.utils.toWei('1', 'ether'), from: accounts[4] });
       const lottery = await lotteryContract.getLottery(1);
       assert.equal(lottery.completed, true);
     })
 
-    it("should transfer the correct amounts", async () => {
-      const lotteryContract = await LotteryTicket.deployed();
-      const winnerAddress = await lotteryContract.getTicketHolders(1)[0];
-      const winningAddressBalance = await Web3.eth.getBalance(winnerAddress);
-      assert.equal(winningAddressBalance, Web3.utils.toWei('3.7', 'ether'));
+    it("should transfer the correct amounts to the winner", async () => {
+      const winningAddressBalance = await web3.eth.getBalance(accounts[2]);
+      assert.equal(winningAddressBalance, Web3.utils.toWei('101.7', 'ether'));
+    })
+
+    it("should transfer the correct amounts to the owner", async () => {
+      const ownerAddressBalance = await web3.eth.getBalance(accounts[1]);
+      assert.equal(ownerAddressBalance, Web3.utils.toWei('100.3', 'ether'));
     })
 });
